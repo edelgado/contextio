c20a8a2a27db98f1b0a2ecfb3570fdfda8b5beb2
Enrique Delgado <delgado.enrique@gmail.com>
2013-08-23 17:16:55 -0500

    Added Context.io and request node packages. Playing with some hard coded Context.io calls.

---
+++ main.js
@@ -0,0 +1,48 @@
+//var request = require('request');
+//request('http://www.google.com', function (error, response, body) {
+//  if (!error && response.statusCode == 200) {
+//    console.log(body); // Print the google web page.
+//  }
+//});
+
+var ContextIO = require('contextio');
+
+var ctxio = new ContextIO.Client({
+  key: 'i6zld4eq',
+  secret: 'qBfGjP55HVxqIDPx'
+});
+
+
+//ctxio.accounts().get({
+//  first_name: 'Catch',
+//  last_name: 'All'
+//}, function (r) {
+//  console.log(r);
+//  ctxio.accounts().get({limit: 10}, function (response) {
+//    console.log(response);
+//  });
+//});
+
+ctxio.accounts().get({limit: 15}, function (err, response) {
+  if (err) throw err;
+  console.log("======= ACCOUNTS ========\n");
+  console.log(response.body);
+});
+
+//ctxio.accounts('5143451dcea6f99d7a000001').messages().get({limit: 15}, function (err, response) {
+//  if (err) throw err;
+//  console.log("===============\n");
+//  console.log(response.body);
+//});
+
+ctxio.accounts('5143451dcea6f99d7a000001').messages().get({limit: 15}, function (err, response) {
+  if (err) throw err;
+  console.log("====== MESSAGES =========\n");
+  console.log(response.body);
+});
+
+ctxio.accounts('5143451dcea6f99d7a000001').messages('520e9ebe8168d96637000002').body().get({}, function (err, response) {
+  if (err) throw err;
+  console.log("======= BODY ========\n");
+  console.log(response.body);
+});
